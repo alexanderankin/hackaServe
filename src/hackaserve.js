3dc6abe23d1a6db74e8f289579cde6f56e8fc5a9
nergmada <adie9@btinternet.com>
2016-10-29 21:18:29 +0100

    removed build directory from hackaserve repo

--- src/hackaserve.js
+++ src/hackaserve.js
@@ -13,9 +13,7 @@ program
 program
     .version('0.0.1')
     .command('send [message_name]')
-    .description('initialise a new hackathon')
-    .option("-x, --example_yml", "generate example yaml that shows how to add attendees")
-    .option("-t, --twilio_key <key>", "A twilio key is required in order to send text messages")
+    .description('Send message to attendees')
     .action(send);
 
 program.parse(process.argv);
